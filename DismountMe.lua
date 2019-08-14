c578135af4ca446a044556e109b40cc7bdbd67ed
fadichmn <fadi@artend.se>
2019-08-14 11:32:47 +0200

    Update DismountMe.lua
    Updated Quriable buff effects from API 8.X

--- DismountMe.lua
+++ DismountMe.lua
@@ -1,15 +1,36 @@
 DismountMe = CreateFrame("Frame")
 DismountMe:RegisterEvent("UI_ERROR_MESSAGE")
 
-DismountMe.Buffs = { "spell_nature_swiftness", "_mount_", "_qirajicrystal_",
-  "ability_racial_bearform", "ability_druid_catform", "ability_druid_travelform",
-  "spell_nature_forceofnature", "ability_druid_aquaticform", "spell_nature_spiritwolf" }
 
+-- Quriable buff effects from https://wowwiki.fandom.com/wiki/Queriable_buff_effects
+DismountMe.Buffs = {
+  "Spell_Nature_Swiftness", -- Warlock Summon Felsteed Mount
+  "Ability_Mount_Dreadsteed", -- Warlock Summon Dreadsteed Mount
+  "_Mount_", --Other Mounts
+  
+  "Ability_Racial_Bearform", -- Druid Bear form
+  "Ability_Druid_Catform",-- Druid Cat form
+  "Ability_Druid_Travelform", -- Druid Travel form
+  "Ability_Druid_Aquaticform", -- Druid Aquatic form
+  "Spell_Nature_Spiritwolf", -- Shaman Spirit Wolf
+  
+  "Ability_Mount_BlackDireWolf", -- Black Dire Wolf Mount
+  "Ability_Mount_WhiteDireWolf", -- White Dire Wolf Mount
+  "Ability_Mount_Kodo_01", -- Kodo 01 Mount
+  "Ability_Mount_Kodo_03", -- Kodo 01 Mount
+  "Ability_Mount_Raptor", -- Raptor Mount
+  "Ability_Mount_Undeadhorse", -- Undead Horse Mount
+  "Ability_Mount_JungleTiger", -- Jungle Tiger Mount
+  "Ability_Mount_WhiteTiger", -- White TigerMount
+  "Ability_Mount_PinkTiger" -- Pink Tiger Mount
+}
+-- Show Errors
 DismountMe.Errors = { SPELL_FAILED_NOT_MOUNTED, ERR_ATTACK_MOUNTED, ERR_TAXIPLAYERALREADYMOUNTED,
   SPELL_FAILED_NOT_SHAPESHIFT, SPELL_FAILED_NO_ITEMS_WHILE_SHAPESHIFTED, SPELL_NOT_SHAPESHIFTED,
   SPELL_NOT_SHAPESHIFTED_NOSPACE, ERR_CANT_INTERACT_SHAPESHIFTED, ERR_NOT_WHILE_SHAPESHIFTED,
   ERR_NO_ITEMS_WHILE_SHAPESHIFTED, ERR_TAXIPLAYERSHAPESHIFTED,ERR_MOUNT_SHAPESHIFTED }
 
+-- Function
 DismountMe:SetScript("OnEvent", function()
     for id, errorstring in pairs(DismountMe.Errors) do
       if arg1 == errorstring then
