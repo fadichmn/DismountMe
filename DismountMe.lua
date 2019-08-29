94605571924cf34972f691c27ff0a26d9d3054c2
fadichmn <fadi@artend.se>
2019-08-29 12:02:29 +0200

    Update DismountMe.lua

--- DismountMe.lua
+++ DismountMe.lua
@@ -1,50 +1,28 @@
-DismountMe = CreateFrame("Frame")
-DismountMe:RegisterEvent("UI_ERROR_MESSAGE")
+local f = CreateFrame("Frame", "LetMeCast", UIParent)
+f:RegisterEvent("UI_ERROR_MESSAGE")
+f:RegisterEvent("TAXIMAP_OPENED")
 
+f:SetScript("OnEvent", function(self, event, arg1, arg2, ...)
+	--print("arg1:", arg1, "| arg2:", arg2)
+	if event == "UI_ERROR_MESSAGE" then
+		if arg1 == 50 then
+			if arg2 == "You must be standing to do that" then
+				DoEmote("STAND")
+			elseif arg2 == "You are mounted" then
+				Dismount()
+			end
+		elseif arg1 == 213 then
+			if arg2 == "You are already mounted! Dismount first." then
+				Dismount()
+			end
+		elseif arg1 == 504 then
+			if arg2 == "You can't do that while mounted." then
+				Dismount()
+			end
+		end
+	elseif event == "TAXIMAP_OPENED" then
+		Dismount()
+	end
+end)
 
--- Quriable buff effects from https://wowwiki.fandom.com/wiki/Queriable_buff_effects
-DismountMe.Buffs = {
-  "Spell_Nature_Swiftness", -- Warlock Summon Felsteed Mount
-  "Ability_Mount_Dreadsteed", -- Warlock Summon Dreadsteed Mount
-  "_Mount_", --Other Mounts
-  
-  "Ability_Racial_Bearform", -- Druid Bear form
-  "Ability_Druid_Catform",-- Druid Cat form
-  "Ability_Druid_Travelform", -- Druid Travel form
-  "Ability_Druid_Aquaticform", -- Druid Aquatic form
-  "Spell_Nature_Spiritwolf", -- Shaman Spirit Wolf
-  
-  "Ability_Mount_BlackDireWolf", -- Black Dire Wolf Mount
-  "Ability_Mount_WhiteDireWolf", -- White Dire Wolf Mount
-  "Ability_Mount_Kodo_01", -- Kodo 01 Mount
-  "Ability_Mount_Kodo_03", -- Kodo 03 Mount
-  "Ability_Mount_Raptor", -- Raptor Mount
-  "Ability_Mount_Undeadhorse", -- Undead Horse Mount
-  "Ability_Mount_JungleTiger", -- Jungle Tiger Mount
-  "Ability_Mount_WhiteTiger", -- White TigerMount
-  "Ability_Mount_PinkTiger" -- Pink Tiger Mount
-}
--- Show Errors
-DismountMe.Errors = { SPELL_FAILED_NOT_MOUNTED, ERR_ATTACK_MOUNTED, ERR_TAXIPLAYERALREADYMOUNTED,
-  SPELL_FAILED_NOT_SHAPESHIFT, SPELL_FAILED_NO_ITEMS_WHILE_SHAPESHIFTED, SPELL_NOT_SHAPESHIFTED,
-  SPELL_NOT_SHAPESHIFTED_NOSPACE, ERR_CANT_INTERACT_SHAPESHIFTED, ERR_NOT_WHILE_SHAPESHIFTED,
-  ERR_NO_ITEMS_WHILE_SHAPESHIFTED, ERR_TAXIPLAYERSHAPESHIFTED,ERR_MOUNT_SHAPESHIFTED }
-
--- Function
-DismountMe:SetScript("OnEvent", function()
-    for id, errorstring in pairs(DismountMe.Errors) do
-      if arg1 == errorstring then
-        for i=0,15,1 do
-          currBuffTex = GetPlayerBuffTexture(i);
-          if (currBuffTex) then
-            for id, bufftype in pairs(DismountMe.Buffs) do
-              if string.find(string.lower(currBuffTex), bufftype) then
-                CancelPlayerBuff(i);
-              end
-            end
-          end
-        end
-      end
-    end
-  end)
-
+f:Hide()
